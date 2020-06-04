323c889f87a52c4639d00a13c3503b16508ec876
Piterden <efremov.a.denis@gmail.com>
2020-06-04 09:05:14 +0300

    Added copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 const path = require('path')
 const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
-// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -69,19 +70,15 @@ const webpackConfig = {
 
   plugins: [
     new webpack.NamedModulesPlugin(),
-    // new CopyWebpackPlugin([
-    //   {
-    //     patterns: [
-    //       {
-    //         from: resolve('src/chrome/manifest.json'),
-    //         to: resolve('dist'),
-    //         toType: 'dir',
-    //       },
-    //     ],
-    //     options: {},
-    //   },
-    // ]),
-    // new HtmlWebpackPlugin({
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: resolve('src/chrome/manifest.json'),
+          to: resolve('dist'),
+        },
+      ],
+    }),
+    // new HTMLWebpackPlugin({
     //   filename: 'index.html',
     //   template: 'index.html',
     //   inject: true,

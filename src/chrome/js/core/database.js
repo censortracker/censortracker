4d1675b9519df39bdd36241cc768e970c0336e1f
Piterden <efremov.a.denis@gmail.com>
2020-06-06 04:46:15 +0300

    Rid of localforage

--- src/chrome/js/core/database.js
+++ src/chrome/js/core/database.js
@@ -1,28 +1,26 @@
-import localforage from 'localforage'
-
-const DEFAULT_DB_NAME = 'censortracker-db'
-
 class Database {
-  constructor (name = DEFAULT_DB_NAME, config = { driver: localforage.INDEXEDDB }) {
-    localforage.config(config)
-    this.name = name
-    this.db = localforage.createInstance({ name })
+  get (key, defaultValue) {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.get(key, (result) => {
+          resolve(result)
+        })
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 
-  async get (key, defaultValue) {
-    const result = await this.db.getItem(key)
-
-    return result === null ? defaultValue : result
-  }
-
-  async set (key, value) {
-    await this.db.setItem(key, value)
-
-    return this
-  }
-
-  async delete () {
-    await localforage.dropInstance({ name: this.name })
+  set (key, value) {
+    return new Promise((resolve, reject) => {
+      try {
+        chrome.storage.local.set({ [key]: value }, () => {
+          resolve({ [key]: value })
+        })
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 }
 

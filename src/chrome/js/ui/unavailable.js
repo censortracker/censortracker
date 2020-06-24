796b2aa904d42340041183a27e33937dc0974626
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-06-24 13:23:56 +0300

    Add proxy_unavailable page

--- src/chrome/js/ui/unavailable.js
+++ src/chrome/js/ui/unavailable.js
@@ -1,31 +1,33 @@
-const extendProxyAutoConfigId = 'extendProxyAutoConfig'
+document.addEventListener('click', (event) => {
+  const buyVpnUrl = 'https://vpnlove.me'
 
-document.addEventListener(
-  'click',
-  (event) => {
-    chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    }, ([tab]) => {
-      const [, encodedHostname] = tab.url.split('?')
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+    const [, encodedHostname] = tab.url.split('?')
+
+    if (event.target.matches('#extendProxyAutoConfig')) {
       const targetUrl = window.atob(encodedHostname)
 
-      if (event.target.matches(`#${extendProxyAutoConfigId}`)) {
-        chrome.tabs.create({ url: targetUrl, index: tab.index }, () => {
-          chrome.tabs.remove(tab.id)
-        })
-      }
-
-      if (event.target.matches('#exitUnavailablePage')) {
+      chrome.tabs.create({ url: targetUrl, index: tab.index }, () => {
         chrome.tabs.remove(tab.id)
-      }
-    })
+      })
+    }
 
-    event.preventDefault()
-  },
-  false,
-)
+    if (event.target.matches('#buyVPN')) {
+      chrome.tabs.create({ url: buyVpnUrl })
+    }
+
+    if (event.target.matches('#closeTab')) {
+      chrome.tabs.remove(tab.id)
+    }
+  })
+
+  event.preventDefault()
+}, false)
 
 setTimeout(() => {
-  document.getElementById(extendProxyAutoConfigId).classList.remove('btn-disabled')
-}, 1000)
+  const extendProxyButton = document.getElementById('extendProxyAutoConfig')
+
+  if (extendProxyButton && extendProxyButton.classList.contains('btn-disabled')) {
+    extendProxyButton.classList.remove('btn-disabled')
+  }
+}, 1200)

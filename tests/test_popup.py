dcbb5e9153f5840a14813bb5acc4c4be1d1d7bdf
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-07-27 15:50:49 +0300

    Refactor tests

--- tests/test_popup.py
+++ tests/test_popup.py
@@ -1,20 +1,28 @@
-from unittest import TestCase
+import unittest
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-class TestPopup(TestCase):
+class TestDistributors(unittest.TestCase):
 
     def setUp(self):
         options = webdriver.ChromeOptions()
-        options.add_extension('censortracker.crx')
+        options.add_extension('./tests/bin/censortracker.crx')
         self.driver = webdriver.Chrome(chrome_options=options)
+        self.chrome_ext_url = 'chrome-extension://kcjbdcgbeblfmjphklkmbonkkmfoffhk/'
 
     def test_ori_found(self):
-        driver = self.driver
-        driver.get('https://2ch.hk')
-        self.assertIn("Python", driver.title)
+        self.driver.get('https://2ch.hk')
+        self.assertIn("Два.ч", self.driver.title)
+
+        self.driver.get(f'{self.chrome_ext_url}/popup.html')
+        self.assertIn('Censor Tracker', self.driver.title)
 
     def tearDown(self):
         self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()

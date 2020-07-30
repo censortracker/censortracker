82de94e6294bc2812d5c7b75161fcd0d2bb15369
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-07-30 19:47:12 +0300

    Fix issues in buildDriver

--- tests/js/background.sepc.js
+++ tests/js/background.sepc.js
@@ -3,23 +3,25 @@ import { Builder } from 'selenium-webdriver'
 const chrome = require('selenium-webdriver/chrome')
 const fs = require('fs')
 
-function getEncodedExtension () {
-  const stream = fs.readFileSync('tests/bin/dist.crx')
+const buildDriver = async () => {
+  const crxData = fs.readFileSync('tests/bin/dist.crx')
+  // eslint-disable-next-line new-cap
+  const extension = new Buffer.from(crxData).toString('base64')
 
-  return new Buffer.from(stream).toString('base64')
-}
+  const options = new chrome.Options()
+  options.addExtensions(extension)
 
-const options = new chrome.Options()
-options.addExtensions(getEncodedExtension())
-
-
-it('Some test', async () => {
-  const browser = await new Builder()
+  const driver = await new Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
     .build()
 
+  return driver
+}
+
+it('Some test', async () => {
+  const browser = await buildDriver()
+
   await browser.get('https://google.com/')
   await browser.quit()
 }, 15000)
-

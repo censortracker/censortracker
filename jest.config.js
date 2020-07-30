2d4e96e95e588464c1522e3eb31d2a756aad0ea9
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-07-30 19:55:09 +0300

    Move driver builder to selenium/

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   verbose: true,
   testMatch: ['**/tests/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  modulePathIgnorePatterns: ['selenium']
 };
--- tests/js/background.sepc.js
+++ tests/js/background.sepc.js
@@ -1,23 +1,4 @@
-import { Builder } from 'selenium-webdriver'
-
-const chrome = require('selenium-webdriver/chrome')
-const fs = require('fs')
-
-const buildDriver = async () => {
-  const crxData = fs.readFileSync('tests/bin/dist.crx')
-  // eslint-disable-next-line new-cap
-  const extension = new Buffer.from(crxData).toString('base64')
-
-  const options = new chrome.Options()
-  options.addExtensions(extension)
-
-  const driver = await new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build()
-
-  return driver
-}
+import buildDriver from "../selenium";
 
 it('Some test', async () => {
   const browser = await buildDriver()

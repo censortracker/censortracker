358d58732297b949056950e910ee7595d1de107b
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-10-07 12:05:02 +0300

    Fix #133

--- src/chrome/js/ui/controlled.js
+++ src/chrome/js/ui/controlled.js
@@ -1,12 +1,55 @@
 (() => {
   const backToPopup = document.getElementById('backToPopup')
-  const extensionsPage = document.getElementById('chromeExtensionsPage')
+  const extensionsLink = document.getElementsByClassName('extensions__link')
+  const extensionNameElements = document.getElementsByClassName('extension__name')
+  const extensionsWhichControlsProxy = document.getElementById('extensionsWhichControlsProxy')
+  const controlledByExtension = document.getElementById('controlledByExtension')
+  const controlledByExtensions = document.getElementById('controlledByExtensions')
+
+  const createExtensionLink = (name) => `<a class="extensions__link">${name}</a>`
+
+  chrome.runtime.getBackgroundPage(async ({ censortracker: bg }) => {
+    const self = await bg.asynchrome.management.getSelf()
+    const extensions = await bg.asynchrome.management.getAll()
+
+    const extensionsWithProxyPermission = extensions.filter(({ name, permissions }) => {
+      return permissions.includes('proxy') && name !== self.name
+    })
+
+    if (extensionsWithProxyPermission.length > 1) {
+      controlledByExtensions.hidden = false
+      controlledByExtension.hidden = true
+      let result = ''
+
+      for (const { name, shortName } of extensionsWithProxyPermission) {
+        result += `<li>${createExtensionLink(shortName || name)}</li>`
+      }
+      extensionsWhichControlsProxy.innerHTML = result
+    } else {
+      controlledByExtension.hidden = false
+      controlledByExtensions.hidden = true
+
+      const extension = extensionsWithProxyPermission[0]
+
+      Array.from(extensionNameElements).forEach((element) => {
+        element.innerText = extension.shortName || extensions.name
+      })
+    }
+
+    Array.from(extensionsLink).forEach((element) => {
+      element.addEventListener('click', () => {
+        chrome.tabs.create({ url: 'chrome://extensions/' })
+      })
+    })
+  })
 
   backToPopup.addEventListener('click', () => {
     window.location.href = chrome.runtime.getURL('popup.html')
   })
 
-  extensionsPage.addEventListener('click', () => {
-    chrome.tabs.create({ url: 'chrome://extensions/' })
-  })
+  const show = () => {
+    document.documentElement.style.visibility = 'initial'
+  }
+
+  setTimeout(show, 100)
 })()

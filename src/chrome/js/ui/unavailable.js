56f4edd304729fd3054bdff5aa791a48551a31e4
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-10-12 09:08:25 +0300

    Fix #139 and #142 (#147)

--- src/chrome/js/ui/unavailable.js
+++ src/chrome/js/ui/unavailable.js
@@ -1,44 +1,42 @@
-// TODO: Use import/from to reduce duplication
+import asynchrome from '../core/asynchrome'
 
-const extendProxyButton = document.getElementById('extendProxyAutoConfig')
+(async () => {
+  const unavailableWebsite = document.getElementById('unavailableWebsite')
+  const extendProxyButton = document.getElementById('extendProxyAutoConfig')
 
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+  const [tab] = await asynchrome.tabs.query({ active: true, lastFocusedWindow: true })
+
   const [, encodedHostname] = tab.url.split('?')
+  const targetUrl = window.atob(encodedHostname)
 
-  if (encodedHostname) {
+  unavailableWebsite.innerText = window.atob(encodedHostname)
+
+  if (encodedHostname && extendProxyButton) {
     extendProxyButton.classList.remove('btn-hidden')
   }
-})
 
-document.addEventListener('click', (event) => {
-  const buyVpnUrl = 'https://vpnlove.me'
-
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
-    const [, encodedHostname] = tab.url.split('?')
-
+  document.addEventListener('click', (event) => {
     if (event.target.matches('#extendProxyAutoConfig')) {
-      const targetUrl = window.atob(encodedHostname)
-
       chrome.tabs.create({ url: targetUrl, index: tab.index }, () => {
         chrome.tabs.remove(tab.id)
       })
     }
 
-    if (event.target.matches('#buyVPN')) {
-      chrome.tabs.create({ url: buyVpnUrl })
+    if (event.target.matches('#tryAgain')) {
+      chrome.tabs.update(tab.id, { url: targetUrl })
     }
 
     if (event.target.matches('#closeTab')) {
       chrome.tabs.remove(tab.id)
     }
-  })
 
-  event.preventDefault()
-}, false)
+    event.preventDefault()
+  }, false)
 
-setTimeout(() => {
-  if (extendProxyButton && extendProxyButton.classList.contains('btn-disabled')) {
-    extendProxyButton.classList.remove('btn-disabled')
-    extendProxyButton.disabled = false
-  }
-}, 3500)
+  setTimeout(() => {
+    if (extendProxyButton && extendProxyButton.classList.contains('btn-disabled')) {
+      extendProxyButton.classList.remove('btn-disabled')
+      extendProxyButton.disabled = false
+    }
+  }, 3500)
+})()

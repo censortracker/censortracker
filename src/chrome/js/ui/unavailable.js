6f1c04daee3770036f8278313b4d1d8478d7c32a
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-10-12 08:52:47 +0300

    Fix #139 and #142

--- src/chrome/js/ui/unavailable.js
+++ src/chrome/js/ui/unavailable.js
@@ -1,44 +1,46 @@
-// TODO: Use import/from to reduce duplication
+(async () => {
+  // TODO: Use import/from to reduce duplication
 
-const extendProxyButton = document.getElementById('extendProxyAutoConfig')
-
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
-  const [, encodedHostname] = tab.url.split('?')
-
-  if (encodedHostname) {
-    extendProxyButton.classList.remove('btn-hidden')
-  }
-})
-
-document.addEventListener('click', (event) => {
-  const buyVpnUrl = 'https://vpnlove.me'
+  const unavailableWebsite = document.getElementById('unavailableWebsite')
+  const extendProxyButton = document.getElementById('extendProxyAutoConfig')
 
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
     const [, encodedHostname] = tab.url.split('?')
 
-    if (event.target.matches('#extendProxyAutoConfig')) {
-      const targetUrl = window.atob(encodedHostname)
+    unavailableWebsite.innerText = window.atob(encodedHostname)
 
-      chrome.tabs.create({ url: targetUrl, index: tab.index }, () => {
-        chrome.tabs.remove(tab.id)
-      })
-    }
-
-    if (event.target.matches('#buyVPN')) {
-      chrome.tabs.create({ url: buyVpnUrl })
-    }
-
-    if (event.target.matches('#closeTab')) {
-      chrome.tabs.remove(tab.id)
+    if (encodedHostname) {
+      extendProxyButton.classList.remove('btn-hidden')
     }
   })
 
-  event.preventDefault()
-}, false)
+  document.addEventListener('click', (event) => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+      const [, encodedHostname] = tab.url.split('?')
+      const targetUrl = window.atob(encodedHostname)
 
-setTimeout(() => {
-  if (extendProxyButton && extendProxyButton.classList.contains('btn-disabled')) {
-    extendProxyButton.classList.remove('btn-disabled')
-    extendProxyButton.disabled = false
-  }
-}, 3500)
+      if (event.target.matches('#extendProxyAutoConfig')) {
+        chrome.tabs.create({ url: targetUrl, index: tab.index }, () => {
+          chrome.tabs.remove(tab.id)
+        })
+      }
+
+      if (event.target.matches('#tryAgain')) {
+        chrome.tabs.update(tab.id, { url: targetUrl })
+      }
+
+      if (event.target.matches('#closeTab')) {
+        chrome.tabs.remove(tab.id)
+      }
+    })
+
+    event.preventDefault()
+  }, false)
+
+  setTimeout(() => {
+    if (extendProxyButton && extendProxyButton.classList.contains('btn-disabled')) {
+      extendProxyButton.classList.remove('btn-disabled')
+      extendProxyButton.disabled = false
+    }
+  }, 3500)
+})()

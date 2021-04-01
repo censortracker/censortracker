d68ef32d1646fffd849cb31d66ff4ed005190fc7
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-04-01 16:33:38 +0300

    Use single settings.js file for both browsers

--- src/chrome/js/core/index.js
+++ src/chrome/js/core/index.js
@@ -3,8 +3,8 @@ export { default as errors } from '../../../common/js/errors'
 export { default as storage } from '../../../common/js/storage'
 export { default as ignore } from '../../../common/js/ignore'
 export { default as registry } from '../../../common/js/registry'
+export { default as settings } from '../../../common/js/settings'
 export { default as proxy } from './proxy'
-export { default as settings } from './settings'
 export {
   enforceHttpConnection,
   enforceHttpsConnection,

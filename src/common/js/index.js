6ef5dd1232b94dc2f95e7e71919741cd16ad22a8
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-05-05 21:57:33 +0300

    Add ipfs modulle

--- src/common/js/index.js
+++ src/common/js/index.js
@@ -4,6 +4,7 @@ export { default as ignore } from './ignore'
 export { default as registry } from './registry'
 export { default as settings } from './settings'
 export { default as proxy } from './proxy'
+export { default as ipfs } from './ipfs'
 export {
   enforceHttpConnection,
   enforceHttpsConnection,

3d5291d5bd3572837013c7deddd34de9ea248662
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-05-31 12:11:37 +0300

    Update message

--- src/common/js/ui/options.js
+++ src/common/js/ui/options.js
@@ -1,7 +1,21 @@
-import { settings, storage } from '@/common/js'
+import { proxy, settings, storage } from '@/common/js'
 
 (async () => {
   const showNotificationsCheckbox = document.getElementById('showNotificationsCheckbox')
+  const openBrowserSettings = document.getElementById('openBrowserSettings')
+  const grantPrivateWindowsPermissionButton = document.getElementById('grantPrivateWindowsPermissionButton')
+  const privateWindowsPermissionRequiredMessage = document.getElementById('privateWindowsPermissionRequiredMessage')
+  const howToGrantPrivateWindowsPermission = document.getElementById('howToGrantPrivateWindowsPermission')
+
+  grantPrivateWindowsPermissionButton.addEventListener('click', async () => {
+    const proxySet = await proxy.setProxy()
+
+    if (proxySet === true) {
+      window.location.href = browser.runtime.getURL('popup.html')
+    } else {
+      howToGrantPrivateWindowsPermission.style.color = 'hsl(348, 100%, 61%)'
+    }
+  })
 
   showNotificationsCheckbox.addEventListener(
     'change', async () => {

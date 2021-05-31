42a3bb5c0ffb8d6792fb5b0129bf176604796cdb
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-05-31 14:28:45 +0300

    Fix options

--- src/common/js/ui/options.js
+++ src/common/js/ui/options.js
@@ -2,20 +2,27 @@ import { proxy, settings, storage } from '@/common/js'
 
 (async () => {
   const showNotificationsCheckbox = document.getElementById('showNotificationsCheckbox')
-  // const openBrowserSettings = document.getElementById('openBrowserSettings')
   const grantPrivateWindowsPermissionButton = document.getElementById('grantPrivateWindowsPermissionButton')
-  // const privateWindowsPermissionRequiredMessage = document.getElementById('privateWindowsPermissionRequiredMessage')
+  const privateWindowsPermissionRequiredMessage = document.getElementById('privateWindowsPermissionRequiredMessage')
   const howToGrantPrivateWindowsPermission = document.getElementById('howToGrantPrivateWindowsPermission')
 
-  grantPrivateWindowsPermissionButton.addEventListener('click', async () => {
-    const proxySet = await proxy.setProxy()
+  if (settings.isFirefox) {
+    const { privateWindowsPermissionRequired } = await storage.get({ privateWindowsPermissionRequired: false })
 
-    if (proxySet === true) {
-      window.location.href = browser.runtime.getURL('popup.html')
-    } else {
-      howToGrantPrivateWindowsPermission.style.color = 'hsl(348, 100%, 61%)'
+    if (privateWindowsPermissionRequired === true) {
+      privateWindowsPermissionRequiredMessage.hidden = false
+
+      grantPrivateWindowsPermissionButton.addEventListener('click', async () => {
+        const proxySet = await proxy.setProxy()
+
+        if (proxySet === true) {
+          privateWindowsPermissionRequiredMessage.hidden = true
+        } else {
+          howToGrantPrivateWindowsPermission.style.color = 'hsl(348, 100%, 61%)'
+        }
+      })
     }
-  })
+  }
 
   showNotificationsCheckbox.addEventListener(
     'change', async () => {

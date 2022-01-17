82a0c9fa10e028c4624e859e030d16d814367cfa
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-01-17 21:05:08 +0300

    Add handlers.js module

---
+++ src/common/js/handlers.js
@@ -0,0 +1,75 @@
+import {
+  enforceHttpsConnection,
+  ignore,
+  proxy,
+  registry,
+  settings,
+  storage,
+} from '.'
+
+/**
+ * Fires when a request is about to occur. This event is sent before any TCP
+ * connection is made and can be used to cancel or redirect requests.
+ * @param url Current URL address.
+ * @returns {undefined|{redirectUrl: *}} Undefined or redirection to HTTPS.
+ */
+export const handleBeforeRequestRedirectToHttps = ({ url }) => {
+  console.warn(`Request redirected to HTTPS for ${url}`)
+
+  if (ignore.contains(url)) {
+    return undefined
+  }
+
+  return {
+    redirectUrl: enforceHttpsConnection(url),
+  }
+}
+
+export const handlerBeforeRequestPing = (_details) => {
+  proxy.ping()
+}
+
+export const handleWindowRemoved = async (_windowId) => {
+  await storage.remove(['notifiedHosts'])
+}
+
+export const handleUninstalled = async (_info) => {
+  await storage.clear()
+}
+
+export const handleStartup = async () => {
+  await registry.sync()
+}
+
+export const handleEnableExtension = async ({ enableExtension: { newValue, oldValue } = {} }) => {
+  if (newValue === true && oldValue === false) {
+    await proxy.setProxy()
+  }
+
+  if (newValue === false && oldValue === true) {
+    await proxy.removeProxy()
+  }
+}
+
+export const handleOptionsChange = async ({ enableExtension, ignoredHosts, useProxy }, _areaName) => {
+  if (ignoredHosts && ignoredHosts.newValue) {
+    ignore.save()
+  }
+
+  if (useProxy && enableExtension === undefined) {
+    const newValue = useProxy.newValue
+    const oldValue = useProxy.oldValue
+
+    const extensionEnabled = settings.extensionEnabled()
+
+    if (extensionEnabled) {
+      if (newValue === true && oldValue === false) {
+        await proxy.setProxy()
+      }
+
+      if (newValue === false && oldValue === true) {
+        await proxy.removeProxy()
+      }
+    }
+  }
+}

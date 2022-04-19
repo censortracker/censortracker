62f119f3bd469ddc4f96646b3e949c0682af8e54
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-04-19 22:43:46 +0300

    Fix utilities

--- src/common/js/utilities.js
+++ src/common/js/utilities.js
@@ -80,14 +80,3 @@ export const extractDecodedOriginUrl = (url, key = 'originUrl') => {
 export const timestamp = () => {
   return Math.floor(Date.now() / 1000)
 }
-
-/**
- * Get browser api
- */
-export const getBrowser = () => {
-  try {
-    return browser
-  } catch (error) {
-    return chrome
-  }
-}

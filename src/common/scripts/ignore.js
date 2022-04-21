0d6b15145b1bcaad98a229c9b4e2cfd1fe016e3a
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-04-21 12:39:58 +0300

    Fix ignore.js

--- src/common/scripts/ignore.js
+++ src/common/scripts/ignore.js
@@ -1,21 +1,14 @@
 import * as storage from './storage'
 import * as utilities from './utilities'
 
-const IGNORE_SAVE_INTERVAL = (60 * 30) * 1000
 const IGNORE_FETCH_INTERVAL = (60 * 15) * 1000
 const IGNORE_API_ENDPOINT_URI = 'https://app.censortracker.org/api/ignore/'
 
 class Ignore {
   constructor () {
-    this._ignoredHosts = new Set() // TODO: Use storage instead
-
     setInterval(async () => {
-      await this.fetch()
+      await this.fetch() // TODO: Use alarm instead
     }, IGNORE_FETCH_INTERVAL)
-
-    setInterval(async () => {
-      await this.save()
-    }, IGNORE_SAVE_INTERVAL)
   }
 
   async fetch () {
@@ -35,34 +28,17 @@ class Ignore {
     }
   }
 
-  async save () {
-    const { ignoredHosts } = await storage.get({ ignoredHosts: [] })
-
-    for (const hostname of ignoredHosts) {
-      this._ignoredHosts.add(hostname)
-      console.log('All ignored domain saved!')
-    }
-  }
-
   clear = async () => {
-    this._ignoredHosts.clear()
     await storage.set({ ignoredHosts: [] })
   }
 
   async add (url) {
     const hostname = utilities.extractHostnameFromUrl(url)
-
-    console.warn(`Added to ignore: ${url}`)
-
     const { ignoredHosts } = await storage.get({ ignoredHosts: [] })
 
     if (!ignoredHosts.includes(hostname)) {
       ignoredHosts.push(hostname)
       console.warn(`Adding ${hostname} to ignore`)
-    }
-
-    for (const item of ignoredHosts) {
-      this._ignoredHosts.add(item)
     }
 
     await storage.set({ ignoredHosts })

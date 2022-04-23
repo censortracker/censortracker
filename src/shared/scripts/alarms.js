394eb805368b0657c522e5f03b68b876756e6db5
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-04-23 17:22:25 +0300

    Fix background.js

--- src/shared/scripts/alarms.js
+++
@@ -1,9 +0,0 @@
-import Browser from './webextension'
-
-export const create = ({ name, periodInMinutes }) => {
-  Browser.alarms.create(name, { periodInMinutes })
-}
-
-export const clear = (name) => {
-  Browser.alarms.clear(name)
-}

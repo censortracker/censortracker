d2c120afc7a02e9f1f703833e093ffe23b206d99
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-05-04 16:29:50 +0300

    Add docstrings for task.js

--- src/shared/scripts/task.js
+++ src/shared/scripts/task.js
@@ -1,14 +1,24 @@
 import Browser from './webextension'
 
 class Task {
+  /**
+   * Create a task and delay it for X minutes.
+   * @param name Task name.
+   * @param minutes Delay in minutes.
+   */
   delay (name, { minutes }) {
     Browser.alarms.create(name, { delayInMinutes: minutes })
     console.warn(`Task.delay('${name}', { minutes: ${minutes} })`)
   }
 
+  /**
+   * Creates a task and schedules it to run every X minutes.
+   * @param name Task name.
+   * @param minutes Periodicity in minutes
+   */
   schedule (name, { minutes }) {
-    Browser.alarms.get(name).then((alarm = {}) => {
-      if (!('name' in alarm)) {
+    Browser.alarms.get(name).then((alarm = { name }) => {
+      if (!name) {
         Browser.alarms.create(name, { periodInMinutes: minutes })
         console.warn(`Task.schedule('${name}', { minutes: ${minutes} })`)
       } else {

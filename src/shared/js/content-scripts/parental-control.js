855f36c26a0fa2c2d7e8f1c216cfd795e05ef9aa
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-06-21 01:19:17 +0300

    Use cached images for parental control

--- src/shared/js/content-scripts/parental-control.js
+++ src/shared/js/content-scripts/parental-control.js
@@ -1,11 +1,5 @@
 (() => {
-  const getBackgroundImageURL = () => {
-    const filename = Math.floor(Math.random() * 5) + 1
-    const githubBaseHost = 'https://raw.githubusercontent.com'
-
-    return `${githubBaseHost}/roskomsvoboda/assets/v1/${filename}.svg`
-  }
-
+  const filename = Math.floor(Math.random() * 5) + 1
   const port = chrome.runtime.connect({ name: 'censortracker' })
 
   // Ask if parental control is isEnabled.
@@ -15,15 +9,8 @@
       const overlay = document.createElement('div')
 
       overlay.id = 'ct-parental-control-overlay'
-      overlay.style.backgroundImage = `url(${getBackgroundImageURL()})`
+      overlay.style.backgroundImage = `url("https://roskomsvoboda.github.io/assets/${filename}.svg")`
       document.body.prepend(overlay)
     }
   })
-
-  const first = 0
-  const second = 0
-
-  if (first >= second) {
-    console.log(`${first} >= ${second} A:E a:e`)
-  }
 })()

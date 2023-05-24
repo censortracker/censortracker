9d5e5931a035820e5c56044a7f9c78001b85e491
Isaak Uchakaev <likid.geimfari@gmail.com>
2023-05-24 19:14:55 +0300

    Add dark theme support for rules editor

--- src/shared/js/pages/rules-editor.js
+++ src/shared/js/pages/rules-editor.js
@@ -3,6 +3,7 @@ import 'codemirror/addon/search/matchesonscrollbar'
 import 'codemirror/addon/search/searchcursor'
 import 'codemirror/addon/display/autorefresh'
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/ayu-mirage.css'
 
 import Browser from 'Background/browser-api'
 import Ignore from 'Background/ignore'
@@ -14,6 +15,8 @@ import CodeMirror from 'codemirror'
   const textarea = document.getElementById('textarea')
   const saveChangesButton = document.getElementById('saveChanges')
   const isIgnorePage = textarea.dataset.type === 'ignoredDomains'
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+
   const editor = CodeMirror.fromTextArea(
     textarea, {
       autorefresh: true,
@@ -23,16 +26,21 @@ import CodeMirror from 'codemirror'
       styleActiveLine: true,
       styleActiveSelected: true,
       disableSpellcheck: true,
+      theme: prefersDarkScheme.matches ? 'ayu-mirage' : 'default',
     },
   )
+
+  // Handle switching between dark and light mode.
+  prefersDarkScheme.addEventListener('change', (event) => {
+    editor.setOption('theme', event.matches ? 'ayu-mirage' : 'default')
+  })
 
   search.addEventListener('input', () => {
     for (const marker of editor.getAllMarks()) {
       marker.clear()
     }
 
-    const value = search.value
-    const cursor = editor.getSearchCursor(value)
+    const cursor = editor.getSearchCursor(search.value)
 
     while (cursor.findNext()) {
       editor.markText(
@@ -53,6 +61,7 @@ import CodeMirror from 'codemirror'
 
     button.textContent = i18nGetMessage('optionsSavedMessage')
 
+    // Reset the button text after 500ms.
     setTimeout(() => {
       button.textContent = i18nGetMessage('saveChanges')
     }, 500)

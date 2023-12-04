8038c47cba79e84c8dbe7e8cbd322f0e22100deb
Isaak Uchakaev <likid.geimfari@gmail.com>
2023-12-04 22:13:55 +0300

    Fix formatting

--- src/shared/js/background/background.js
+++ src/shared/js/background/background.js
@@ -14,31 +14,52 @@ import {
 
 import browser from './browser-api'
 
+// Handle alarms for async tasks
 browser.alarms.onAlarm.addListener(handleOnAlarm)
+// Handle extension lifecycle events
 browser.runtime.onStartup.addListener(handleStartup)
 browser.runtime.onInstalled.addListener(handleInstalled)
 browser.runtime.onUpdateAvailable.addListener(handleOnUpdateAvailable)
+// Handle tab changes (e.g. new tab, tab closed)
 browser.tabs.onUpdated.addListener(handleTabState)
 browser.tabs.onCreated.addListener(handleTabCreate)
+// Handle storage changes (e.g. settings)
 browser.storage.onChanged.addListener(handleStorageChanged)
 browser.storage.onChanged.addListener(handleIgnoredHostsChange)
 browser.storage.onChanged.addListener(handleCustomProxiedDomainsChange)
 
 if (browser.isFirefox) {
+  // Firefox-specific handlers
   browser.proxy.onError.addListener(handleProxyError)
   browser.webRequest.onBeforeRequest.addListener(
     handleBeforeRequest, {
-      urls: ['http://*/*', 'https://*/*'],
-      types: ['main_frame'],
+      urls: [
+        'http://*/*',
+        'https://*/*',
+      ],
+      types: [
+        'main_frame',
+      ],
     },
   )
-
-  browser.webRequest.onErrorOccurred.addListener(handleProxyError, { urls: ['<all_urls>'] })
+  browser.webRequest.onErrorOccurred.addListener(
+    handleProxyError, {
+      urls: [
+        '<all_urls>',
+      ],
+    },
+  )
 } else {
+  // Chrome-specific handlers
   chrome.webNavigation.onBeforeNavigate.addListener(
     handleBeforeRequest, {
-      urls: ['http://*/*', 'https://*/*'],
-      types: ['main_frame'],
+      urls: [
+        'http://*/*',
+        'https://*/*',
+      ],
+      types: [
+        'main_frame',
+      ],
     },
   )
   chrome.proxy.onProxyError.addListener(handleProxyError)

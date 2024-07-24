f2ab6127ec4dd66560328a998b2d371056cff3b0
ANDREYFROL6262 <97760266+ANDREYFROL6262@users.noreply.github.com>
2024-07-24 04:40:57 +0300

    Unit tests: configuring proxy

---
+++ src/shared/js/tests/unit/stateIndependentBehaviour/proxy.test.js
@@ -0,0 +1,15 @@
+import ProxyManager from '../../../extension/base/proxy'
+import * as mockedStorage from '../../env/mockedStorage/dataAccess'
+
+test('default setting proxy', async () => {
+  const proxyData = await ProxyManager.setProxy()
+
+  expect(proxyData).toBe(true)
+})
+
+test('setting proxy with empty registry', async () => {
+  await mockedStorage.set({ domains: [] })
+  const proxyData = await ProxyManager.setProxy()
+
+  expect(proxyData).toBe(false)
+})

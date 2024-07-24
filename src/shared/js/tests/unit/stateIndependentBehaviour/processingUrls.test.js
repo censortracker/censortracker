805e2470b986d13a13a6628585f08e2a0aeaf11d
ANDREYFROL6262 <97760266+ANDREYFROL6262@users.noreply.github.com>
2024-07-24 04:40:28 +0300

    Unit tests: processing urls

---
+++ src/shared/js/tests/unit/stateIndependentBehaviour/processingUrls.test.js
@@ -0,0 +1,38 @@
+import extension from '../../../extension/base/extension'
+
+test('testing norn=mal host', async () => {
+  const result = await extension.handlers.handleTabState({
+    tabId: 0, status: 'loading', url: 'https://example.com/',
+  })
+
+  expect(result).toBe('normal')
+})
+
+test('testing blocked host', async () => {
+  const result = await extension.handlers.handleTabState({
+    tabId: 0, status: 'loading', url: 'https://www.bbc.com/',
+  })
+
+  expect(result).toBe('blocked')
+})
+
+test('testing blocked host of level 3', async () => {
+  const result = await extension.handlers.handleTabState({
+    tabId: 0, status: 'loading', url: 'https://ru.iherb.com/',
+  })
+
+  expect(result).toBe('blocked')
+})
+
+jest.mock('../../../extension/base/notifications/notifications', () => ({
+  ...jest.requireActual('../../../extension/base/notifications/notifications'),
+  showDisseminatorWarning: (url) => console.log('Notification from:', url),
+}))
+
+test('testing disseminator', async () => {
+  const result = await extension.handlers.handleTabState({
+    tabId: 0, status: 'loading', url: 'https://www.avito.ru/',
+  })
+
+  expect(result).toBe('disseminator')
+})
